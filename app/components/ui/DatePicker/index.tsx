385f779106650d52e6399950b0ef5ef17704e840
hungth <hungth@smartosc.com>
2023-06-20 17:20:15 +0700

    feat: create custome input number, datepicker, stepper, footer and react-youtube

---
+++ app/components/ui/DatePicker/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function BasicDatePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker
+          format="DD-MM-YYYY"
+          slotProps={{
+            layout: {
+              sx: {
+                '.MuiPickersDay-today': {
+                  border: '0 !important',
+                  color: '#E03155',
+                },
+                '.MuiPickersDay-today:focus': {
+                  borderRadius: '8px !important',
+                },
+                '.Mui-selected': {
+                  background: '#E03155',
+                  color: 'white',
+                  borderRadius: '8px',
+                },
+                '.Mui-selected:hover': {
+                  willChange: 'background-color',
+                  backgroundColor: 'rgb(156, 34, 59) !important',
+                },
+                '.MuiPickersDay-root:hover': {
+                  backgroundColor: 'rgba(224, 49, 85, 0.04)',
+                  borderRadius: '8px',
+                },
+              },
+            },
+          }}
+        />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
